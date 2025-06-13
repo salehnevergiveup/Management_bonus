5452b822449804c47b977ce24a5c9261af0bd435
salehnevergiveup <salehaldhaheri09@gmail.com>
2025-06-13 10:33:35 +0800

    red sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -92,7 +92,7 @@ export default function AppSidebar({ className, ...props }: React.ComponentProps
           </Button>
         )}
       </SidebarHeader>
-      <SidebarContent className="overflow-y-auto overflow-x-hidden">
+      <SidebarContent className="overflow-y-auto overflow-x-hidden bg-red-500">
         {/* Dashboard */}
         <SidebarGroup>
           <SidebarGroupContent>
